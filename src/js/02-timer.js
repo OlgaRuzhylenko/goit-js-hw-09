de5357229c4dcdd5453d1acee5bf91c020599318
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-10-26 21:15:27 +0300

    2 завдання

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,22 +3,59 @@ import "flatpickr/dist/flatpickr.min.css";
 
 const myInput = document.querySelector('#datetime-picker');
 const btnStart = document.querySelector('[data-start]');
-const defaultDate = new Date();
+const timer = document.querySelector('.timer');
+const dataDays = document.querySelector('[data-days]');
+const dataHours = document.querySelector('[data-hours]');
+const dataMinutes = document.querySelector('[data-minutes]');
+const dataSeconds = document.querySelector('[data-seconds]');
 
+let defaultDate = new Date();
+
+function updateDefaultDate() {
+    defaultDate = new Date();
+}
+    
+function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    return { days, hours, minutes, seconds };
+} 
+ 
 const fp = flatpickr(myInput, {
-enableTime: true,
-  time_24hr: true,
-  defaultDate: defaultDate,
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-      console.log(selectedDates[0]);
-           if (selectedDates[0] - defaultDate <= 0) {
-               alert('Please choose a date in the future');
-               btnStart.setAttribute('disabled', 'true');
-                  } else {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: defaultDate,
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        console.log(selectedDates[0]);
+        if (selectedDates[0] - defaultDate <= 0) {
+            alert('Please choose a date in the future');
+            btnStart.setAttribute('disabled', 'true');
+        } else {
+            btnStart.removeAttribute('disabled');
+            btnStart.addEventListener('click', onBtnStartClick);
+            function onBtnStartClick() {
                
-               btnStart.removeAttribute('disabled');
-      }
-         },
-
-}); 
+                const intervalId = setInterval(() => {
+                    updateDefaultDate();
+                    const ms = selectedDates[0] - defaultDate;
+                    const result = convertMs(ms);
+                    console.log(result);
+                    
+                    dataDays.textContent = result.days;
+                    dataHours.textContent = result.hours;
+                    dataMinutes.textContent = result.minutes;
+                    dataSeconds.textContent = result.seconds;
+                }, 1000);
+            }
+        }
+    }
+}
+);
