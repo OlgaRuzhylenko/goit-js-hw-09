253ef6b66c81cde1a364cb503a4a3ce70df4e093
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-10-26 13:20:03 +0300

    1 завдання

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let intervalId;
 
+buttonStart.addEventListener('click', onStartClick);
+function onStartClick() {
+        this.setAttribute('disabled', 'true')
+        intervalId = setInterval(() => {
+            const someColor = getRandomHexColor();
+            body.style.backgroundColor = someColor
+        }, 1000);
+    buttonStop.removeAttribute('disabled');
+    };
+
+buttonStop.addEventListener('click', onStopClick);
+function onStopClick() {
+    clearInterval(intervalId);
+    this.setAttribute('disabled', 'true')
+    buttonStart.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
