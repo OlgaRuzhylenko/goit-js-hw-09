2fc084ad591be34c9443e99a2a63d78ca0ebcd29
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2024-02-21 07:11:20 +0000

    Deploying to gh-pages from @ OlgaRuzhylenko/goit-js-hw-09@2f84c55e1dfbdeeb5ec4401220b236239c18ecc1 🚀

---
+++ index.ce62688b.js
@@ -0,0 +1,2 @@
+let e;var o;(o=[1,5,3,4],new Promise(((l,t)=>{setTimeout((()=>{if(o||o.length>0)for(let l=0;l<o.length;l+=1)e=Math.max(o[l]),console.log(e);else t(console.log(err))}),500)}))).then((()=>{console.log(item)})).catch((e=>{console.log("this is ERROR")}));
+//# sourceMappingURL=index.ce62688b.js.map
