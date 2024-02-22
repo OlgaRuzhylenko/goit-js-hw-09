813812d10da37d578daa12d82405fe9f850d7798
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2024-02-22 09:49:07 +0000

    Deploying to gh-pages from @ OlgaRuzhylenko/goit-js-hw-09@e58223912d4b51dddf510acebd05b1dd5c024fb3 🚀

--- index.7ec895ac.js
+++
@@ -1,2 +0,0 @@
-!function(){var o,n;(n=[1,5,3,4],new Promise((function(e,t){setTimeout((function(){if(n||n.length>0)for(var e=0;e<n.length;e+=1)o=Math.max(n[e]),console.log(o);else t(console.log(err))}),500)}))).then((function(){console.log(item)})).catch((function(o){console.log("this is ERROR")}))}();
-//# sourceMappingURL=index.7ec895ac.js.map
