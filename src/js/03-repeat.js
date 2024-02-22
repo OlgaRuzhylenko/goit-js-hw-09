e58223912d4b51dddf510acebd05b1dd5c024fb3
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2024-02-22 11:48:49 +0200

    b

--- src/js/03-repeat.js
+++ src/js/03-repeat.js
@@ -24,28 +24,111 @@
 
 // Пошук максимального елемента в масиві: Напишіть функцію, яка приймає масив чисел та повертає проміс, який вирішується максимальним числом у масиві.
 
-const arr = [1, 5, 3, 4];
-let maxItem;
+// const arr = [1, 5, 3, 4];
 
-function createPromise(arr) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (arr || arr.length > 0) {
-                for (let i = 0; i < arr.length; i += 1){
-                    maxItem = Math.max(arr[i]);
-console.log(maxItem);
-                }
-            } else (reject(console.log(err)))
-        }, 500)
-    })
-}
+// function createPromise(arr) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (arr && arr.length > 0) {
+//                 const maxNumber = Math.max(...arr)
+//                 resolve (maxNumber)
+//             } else {
+//  reject('no result');
+//             }
+//         }, 500)
+//     })
+// }
+// createPromise(arr).then((data) => {
+//     console.log(data);
+// }).catch((err) => {
+//     console.log('Error:', err);
+// })
 
-createPromise(arr).then(() => {
-    console.log(item);
-}).catch((err) => {
-    console.log('this is ERROR');
+
+
+
+
+
+
+
+// Фільтрація масиву: Напишіть функцію, яка приймає масив чисел та значення, і повертає проміс, який вирішується новим масивом,
+// який містить лише числа з початкового масиву, більші за задане значення.
+
+// const arr = [1, 2, 5, 7, 45, 11]
+// let newArr = []
+
+// function createPromise(arr, x) {
+//   return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (arr && arr.length > 0) {
+//                 const newArr = arr.filter(item => item > x);
+//                 resolve(newArr);
+//             } else {
+//                 reject('Array is empty or undefined');
+//             }
+//         }, 500);
+//     });
+// }
+
+// createPromise(arr, 5)
+//     .then(result => {
+//         console.log(result);
+//     }).catch(error => {
+//         console.error('Error:', error);
+//     });
+
+
+
+
+
+
+
+
+
+// Автентифікація користувача за допомогою форми: Напишіть функцію, яка приймає дані з форми автентифікації(наприклад, ім'я користувача та пароль) та зберігає їх локально на пристрої користувача (наприклад, використовуючи localStorage). Поверніть проміс, який вирішується, коли дані успішно збережено.
+
+const formItem = document.querySelector('.form');
+const btn = document.querySelector('button');
+
+let dataObj = JSON.parse(localStorage.getItem('objectInform')) ?? []
+
+
+formItem.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+  
+    const obj = {
+        name: formItem.elements.Name.value,
+        age: formItem.elements.Age.value
+    };
+
+    dataObj.push(obj)
+    formItem.elements.Name.value = '';
+    formItem.elements.Age.value = '';
+
+    localStorage.setItem('objectInform', JSON.stringify(dataObj));
+
+    createPromise().then(() => {
+    console.log(`localeStorage has ${obj.name} and ${obj.age}`);
+}).catch((error) => {
+    console.log('Error:', error);
 })
 
+}
+
+function createPromise() {
+    return new Promise((resolve, reject) => {
+          setTimeout(() => {
+              if (dataObj) {
+            resolve()
+              } else {
+                  reject('Error: Data is not available')
+        }
+    }, 500)
+    })
+  
+}
 
 
 
@@ -63,19 +146,3 @@ createPromise(arr).then(() => {
 
 
 
-
-
-
-
-
-// Фільтрація масиву: Напишіть функцію, яка приймає масив чисел та значення, і повертає проміс, який вирішується новим масивом, який містить лише числа з початкового масиву, більші за задане значення.
-
-// Обчислення середнього арифметичного: Напишіть функцію, яка приймає масив чисел та повертає проміс, який вирішується середнім арифметичним чисел у масиві.
-
-// Пошук унікальних елементів: Напишіть функцію, яка приймає масив чисел та повертає проміс, який вирішується новим масивом, який містить лише унікальні елементи з початкового масиву (без повторень).
-
-// Знаходження суми додатних чисел: Напишіть функцію, яка приймає масив чисел та повертає проміс, який вирішується сумою всіх додатних чисел у масиві.
-
-// Автентифікація користувача за допомогою форми: Напишіть функцію, яка приймає дані з форми автентифікації(наприклад, ім'я користувача та пароль) і повертає проміс, який вирішується, якщо користувач успішно автентифікований, або відхиляється, якщо автентифікація не вдалася.
-
-// Збереження даних локально: Напишіть функцію, яка приймає дані та зберігає їх локально на пристрої користувача (наприклад, використовуючи localStorage). Поверніть проміс, який вирішується, коли дані успішно збережено.
